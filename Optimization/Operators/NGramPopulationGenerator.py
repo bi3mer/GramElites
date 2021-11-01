866a0f0bc714391383b20019888740a04944923c
bi3mer <colan@comcast.net>
2021-11-01 14:03:37 -0400

    working on setup

---
+++ Optimization/Operators/NGramPopulationGenerator.py
@@ -0,0 +1,16 @@
+from itertools import repeat
+from random import choice
+
+class NGramPopulationGenerator:
+    __slots__ = ['strand_size', 'gram']
+
+    def __init__(self, n_gram, strand_size):
+        self.strand_size = strand_size
+        self.gram = n_gram
+
+    def generate(self, n):
+        keys = list(self.gram.grammar.keys())
+        return [
+            self.gram.generate(choice(keys), self.strand_size) 
+            for _ in repeat(None, n)
+        ]
